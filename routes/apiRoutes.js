d32a038666d57ab8286232d559230c4d30557f37
Wong <thatlostboy@gmail.com>
2018-10-26 22:03:37 -0700

    building starter file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,18 @@
+//var db = require("../models");
+
+module.exports = function(app) {
+  // Get all examples
+  app.get("/api/scrape", function(req, res) {
+
+  });
+
+  // get list of all stories
+  app.get("/api/stories", function(req, res) {
+
+  });
+
+  // delete specific storiy
+  app.post("/api/comments/:id", function(req, res) {
+
+  });
+};
---
+++ server.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+});
+
+module.exports = app;
