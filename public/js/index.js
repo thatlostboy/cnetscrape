93c6de217cd92009242828b07b21ffd92c7f4ee0
Wong <thatlostboy@gmail.com>
2018-10-28 10:03:33 -0700

    working on index page.  have dynamic loading up and running

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,98 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
+$(document).ready(function () {
+  console.log("ready!");
+
+  // scrape new Web Page at startup
+  getNewArticles()
+
+  // onclick handlers
+  // get new articles
+  $(document).on("click",".scrapeNewArticles", function(){
+    getNewArticles();
+  })
+
+  // clear all articles on saved database
+  $(document).on("click",".clearArticles", function(){
+    alert("clear was clicked.  will call clear api then call scrape new articles function");
+  })
+
+  // add new articles
+  $(document).on("click",".saveArticle", function(){
+    alert("saved was clicked.  will need to create save function");
+    //https://stackoverflow.com/questions/6647736/how-to-delete-parent-element-using-jquery
+    
+  })
+
+
+});
+
+var articleTemplate = `
+<div class="card text-left tagToDelete">
+<div class="card-header">
+  <h4><a href="{{link}}">{{title}}</a></h4>
+</div>
+<div class="card-body">
+  <p class="card-title">{{summary}}</p>
+  <div class="text-right">
+    <button class="btn btn-success saveArticle">ADD TO SAVED</button>
+  </div>
+</div>
+</div>
+<hr>
+`
+
+var noNewArticlesHTML = `
+<hr>
+<h3>No New Articles... Click on Scrape new LInk to see new articles or click saved page to see saved articles </h3>  
+<hr>
+`
+
+var processingArticlesHTML = `
+<hr>
+<p>Loading Latest Articles... </p>  
+<hr>`
+
+function getNewArticles() {
+
+  $("#articleList").html(processingArticlesHTML)
 
   $.get("/api/scrapeNotSaved")
-    .then(function(data){
+    .then(function (data) {
       console.log(data)
       if (data.length > 0) {
         console.log("I have data!  Will call render function!")
+        updateArticleList(data)
       } else {
         console.log("I do not have data!  will call non render function!")
       }
     })
-});
+}
+
+function updateArticleList(articleArray) {
+  console.log("Listing!")
+  let articleListHTML = ""
+  articleArray.forEach(function (article) {
+    console.log(article.title + "\n" + article.summary + "\n" + article.link)
+
+    // create copy of Template
+    let newArticle = articleTemplate
+
+    // search replace
+    newArticle = newArticle.replace(/{{title}}/, article.title)
+    newArticle = newArticle.replace(/{{link}}/, article.link)
+    newArticle = newArticle.replace(/{{summary}}/, article.summary)
+    console.log(newArticle)
+
+    // append article to list
+    articleListHTML += newArticle
+
+  })
+  // jquery to update article list
+  $("#articleList").html(articleListHTML)
+}
+
+function noNewArticles() {
+  $("#articleList").html(noNewArticlesHTML)
+}
 
 
 
@@ -22,7 +104,7 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -32,13 +114,13 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -47,9 +129,9 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
       var $a = $("<a>")
         .text(example.text)
         .attr("href", "/example/" + example.id);
@@ -77,7 +159,7 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var example = {
@@ -90,7 +172,7 @@ var handleFormSubmit = function(event) {
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(example).then(function () {
     refreshExamples();
   });
 
@@ -100,12 +182,12 @@ var handleFormSubmit = function(event) {
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
